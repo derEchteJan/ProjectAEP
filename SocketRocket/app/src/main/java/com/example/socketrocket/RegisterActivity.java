0b38243712eed8354e9e362181fbf0957345d7c9
derEchteChris <arndtchristian01@t-online.de>
2020-01-15 10:13:44 +0100

    sign ups

--- SocketRocket/app/src/main/java/com/example/socketrocket/RegisterActivity.java
+++ SocketRocket/app/src/main/java/com/example/socketrocket/RegisterActivity.java
@@ -2,12 +2,15 @@ package com.example.socketrocket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.service.autofill.UserData;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +28,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
     private EditText usernameInput, emailInput, passwordInput, confirmPasswordInput;
     private Button signUpButton;
     private int currentRequestId = NetworkRequestDelegate.INVALID_REQUEST_ID;
+    private DatabaseConnection dbHandle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +36,11 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
-        DatabaseConnection.sharedInstance().initDatabase(this);
+        this.dbHandle = new DatabaseConnection(this);
         this.init();
     }
 
     private void init() {
-        // TODO: Email komplett noch dazu + handling
         this.usernameInput = this.findViewById(R.id.register_editText_username);
         this.emailInput = this.findViewById(R.id.register_editText_email);
         this.passwordInput = this.findViewById(R.id.register_editText_password);
@@ -50,6 +53,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
     public void onClick(View v) {
         if(v == this.signUpButton) {
             String username = this.usernameInput.getText().toString();
+            String email = this.emailInput.getText().toString();
             String password = this.passwordInput.getText().toString();
             String repeatPassword = this.confirmPasswordInput.getText().toString();
             if(!checkPasswords(password, repeatPassword)) {
@@ -61,7 +65,11 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
             } else if(!this.checkUsername(username)) {
                 // username falsch -> textfeld löschen, evtl noch rot unterstreichen?
                 this.usernameInput.getText().clear();
-            } else {
+                this.showInvalidUsername(); //Anzeige für Falschen Usernamen aufrufen
+            } else if(!this.checkEmail(email)){
+                // email enthält kein "@" --> textfeld wird gelöscht
+                this.usernameInput.getText().clear();
+            }else {
                 int requestId = NetworkConnection.sendRegistrationRequest(this, "email", username, password);
                 if(requestId == NetworkRequestDelegate.INVALID_REQUEST_ID) {
                     this.currentRequestId = requestId;
@@ -86,29 +94,39 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
         return username.length() > 3;
     }
 
+    private boolean checkEmail(String email) {
+        // TODO: erweitern
+        // liefert ob der username im gültigen format und lang genug ist
+        return email.contains("@");
+    }
 
     // MARK: - Input Feedback
 
     private void showInvalidPassword() {
         // TODO: umsetzen
+        Toast.makeText(this, "Invalid Password!", Toast.LENGTH_LONG).show();
     }
 
     private void showInvalidUsername() {
         // TODO: umsetzen
+        Toast.makeText(this, "Invalid Username!", Toast.LENGTH_LONG).show();
     }
 
     private void showNetworkError(NetworkErrorType errorType) {
         // TODO: umsetzen
         System.out.println("Netzwerkfehler! "+errorType.toString());
+        Toast.makeText(this, errorType.toString(), Toast.LENGTH_LONG).show();
     }
 
     private void showInternalError() {
         // TODO: umsetzen
         System.out.println("Interner Fehler aufgetreten! ");
+        Toast.makeText(this, "Internal Error!", Toast.LENGTH_LONG).show();
     }
 
     private void showSignUpSuccess() {
         // TODO: umsetzen
+        Toast.makeText(this, "Registration completed!", Toast.LENGTH_LONG).show();
     }
 
 
@@ -144,7 +162,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener, 
             newUser.email = (String)responseObject.get("email");
             newUser.password = (String)responseObject.get("password");
             newUser.token = (String)responseObject.get("token");
-            DatabaseConnection.sharedInstance().addUser(newUser);
+            this.dbHandle.addUser(newUser);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
